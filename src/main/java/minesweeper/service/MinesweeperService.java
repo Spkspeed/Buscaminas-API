23bc35c6803ca3011862eef9f14b445d62ff5528
recruz79 <raul.cruz79@gmail.com>
2021-01-19 18:01:50 -0300

    agregando logback envez de log4j

--- src/main/java/minesweeper/service/MinesweeperService.java
+++ src/main/java/minesweeper/service/MinesweeperService.java
@@ -1,6 +1,8 @@
 package minesweeper.service;
 
 import minesweeper.exception.MinesweeperException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,9 @@ public class MinesweeperService {
 
     @Autowired
     MinesweeperPersistService minesweeperPersistService;
+
+    Logger logger = LoggerFactory.getLogger(MinesweeperService.class);
+
 
     protected HashMap<String, MineGameGrid> gameInstances = new HashMap();
 
@@ -69,6 +74,9 @@ public class MinesweeperService {
     }
 
     public void saveGame(String user) throws MinesweeperException {
+        logger.debug("El problema es que devuelve: " + minesweeperPersistService);
+        logger.debug("El usuario es: " + user);
+        logger.debug("Instancias del juego del usuario: " + gameInstances.get(user));
         minesweeperPersistService.saveGame(user, gameInstances.get(user));
     }
 
